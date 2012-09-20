28e1f0c112a402e20580ebedf5bb0825ca4b2577
Almightree <almightree@gmail.com>
2012-09-20 09:15:56 -0700

    New Libs and initial Lua file
    Also added embeds.xml and EasyLOP.xml

---
+++ EasyLOP.lua
@@ -0,0 +1,27 @@
+﻿EasyLOP = LibStub("AceAddon-3.0"):NewAddon("EasyLOP", "AceConsole-3.0", "AceEvent-3.0")
+
+local defaults = {
+  profile = {
+    optionA = true,
+    optionB = false,
+    suboptions = {
+      subOptionA = false,
+      subOptionB = true,
+    },
+  }
+}
+
+
+function EasyLOP:OnInitialize()
+    --AceDB LoadIn
+    self.db = LibStub("AceDB-3.0"):New("EasyLOPDB", dafaults)
+    
+    --Set Default Values
+
+    
+end
+
+function EasyLOP:OnEnable()
+
+end
+
